f8633e0d58d40ed5d18e345d11603dfef45b0c30
maxim <maxim@gigaspaces.com>
2015-05-14 10:44:52 +0300

    Finalizing keep_track capabilities.

--- winrm/__init__.py
+++ winrm/__init__.py
@@ -1,6 +1,7 @@
 import re
 import base64
 import xml.etree.ElementTree as ET
+import sys
 
 from winrm.protocol import Protocol
 
@@ -23,22 +24,26 @@ class Session(object):
         self.url = self._build_url(target, transport)
         self.protocol = Protocol(self.url, transport=transport, username=username, password=password)
 
-    def run_cmd(self, command, args=()):
+    def run_cmd(self, command, args=(), out_stream=sys.stdout, err_stream=sys.stderr, keep_track=False):
         #TODO optimize perf. Do not call open/close shell every time
         shell_id = self.protocol.open_shell()
         command_id = self.protocol.run_command(shell_id, command, args)
-        rs = Response(self.protocol.get_command_output(shell_id, command_id))
+        rs = Response(self.protocol.get_command_output(shell_id, command_id, out_stream, err_stream, keep_track))
         self.protocol.cleanup_command(shell_id, command_id)
         self.protocol.close_shell(shell_id)
         return rs
 
+    def run_ps(self, script, out_stream=sys.stdout, err_stream=sys.stderr, keep_track=False):
+        """base64 encodes a Powershell script and executes the powershell
+        encoded script command
+        """
 
-    def run_ps(self, script):
+    def run_ps(self, script, out_stream=sys.stdout, err_stream=sys.stderr, keep_track=False):
         """base64 encodes a Powershell script and executes the powershell encoded script command"""
 
         # must use utf16 little endian on windows
         base64_script = base64.b64encode(script.encode("utf_16_le"))
-        rs = self.run_cmd("powershell -encodedcommand %s" % (base64_script))
+        rs = self.run_cmd("powershell -encodedcommand %s" % (base64_script), out_stream, err_stream, keep_track)
         if len(rs.std_err):
             # if there was an error message, clean it it up and make it human readable
             rs.std_err = self.clean_error_msg(rs.std_err)
